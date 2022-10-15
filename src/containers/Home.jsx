4fed64b56e9b6471dfb590ef70849d92c0b74669
Victor Carreno <vmanuelcar@hotmail.com>
2022-10-14 20:19:14 -0500

    Fix bug of height property at Home component

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -13,70 +13,72 @@ import cvPdf from '../assets/files/CV - Victor Carreño.pdf'
 const Home = () => {
     const [fall, setFall] = useState("")
 
-  return (
-        <section className='flex flex-col-reverse h-screen px-[7%] justify-center items-center gap-6 sm:gap-10 md:flex-row bg-cream-100' id="home">
-            <div>
-                <h1 className='text-4xl sm:text-5xl font-bold text-center md:text-left'>Victor Manuel Carreño Peña</h1>
-                <h2 className='text-2xl sm:text-3xl my-4 font-bold text-center md:text-left'>Frontend Developer</h2>
-                <div className='w-max mx-auto md:mx-0'>
-                    <p>
-                        <FaRegEnvelope className='inline-block w-6 h-6 border-2 border-amber-900 rounded-md p-0.5 mr-3 my-1' />
-                        vmanuelcar@hotmail.com
-                    </p>
-                    <p>
-                        <FaMapMarkerAlt className='inline-block w-6 h-6 border-2 border-amber-900 rounded-md p-0.5 mr-3 my-1' />
-                        Bogotá D.C, Colombia
-                    </p>
+    return (
+        <section className='h-screen w-full bg-cream-100'>
+            <div className='h-full w-full flex flex-col-reverse px-[7%] justify-center items-center gap-6 sm:gap-10 md:flex-row bg-transparent animate__zoomIn animate__animated animate__delay' id="home">
+                <div>
+                    <h1 className='text-4xl sm:text-5xl font-bold text-center md:text-left'>Victor Manuel Carreño Peña</h1>
+                    <h2 className='text-2xl sm:text-3xl my-4 font-bold text-center md:text-left'>Frontend Developer</h2>
+                    <div className='w-max mx-auto md:mx-0'>
+                        <p>
+                            <FaRegEnvelope className='inline-block w-6 h-6 border-2 border-amber-900 rounded-md p-0.5 mr-3 my-1' />
+                            vmanuelcar@hotmail.com
+                        </p>
+                        <address>
+                            <FaMapMarkerAlt className='inline-block w-6 h-6 border-2 border-amber-900 rounded-md p-0.5 mr-3 my-1' />
+                            Bogotá D.C, Colombia
+                        </address>
+                    </div>
+                    <div className='w-52 mt-3 flex justify-around mx-auto md:mx-0'>
+                        <a
+                            aria-label="Link to Twitter's profile"
+                            href="https://twitter.com/victorcar86_"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaTwitter className='w-6 h-6 hover:scale-110 transition-transform ease-out' />
+                        </a>
+                        <a
+                            aria-label="Link to Github's profile"
+                            href="https://github.com/VictorCar86"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaGithub className='w-6 h-6 hover:scale-110 transition-transform ease-out' />
+                        </a>
+                        <a
+                            aria-label="Link to Linkedin's profile"
+                            href="https://www.linkedin.com/in/victorcar86/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaLinkedin className='w-6 h-6 hover:scale-110 transition-transform ease-out' />
+                        </a>
+                    </div>
+                    <div className='ml-0 md:ml-9 my-4 text-center md:text-left'>
+                        <a href={cvPdf} download>
+                            <button className='text-amber-900 font-bold bg-amber-100 border-2 border-amber-600 rounded-2xl px-3 py-1 shadow-md active:scale-105 hover:scale-105 hover:shadow-lg transition-transform ease-out' type='button'>
+                                Download CV
+                            </button>
+                        </a>
+                    </div>
                 </div>
-                <div className='w-52 mt-3 flex justify-around mx-auto md:mx-0'>
-                    <a
-                        aria-label="Link to Twitter's profile"
-                        href="https://twitter.com/victorcar86_"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <FaTwitter className='w-6 h-6 hover:scale-110 transition-transform ease-out' />
-                    </a>
-                    <a
-                        aria-label="Link to Github's profile"
-                        href="https://github.com/VictorCar86"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <FaGithub className='w-6 h-6 hover:scale-110 transition-transform ease-out' />
-                    </a>
-                    <a
-                        aria-label="Link to Linkedin's profile"
-                        href="https://www.linkedin.com/in/victorcar86/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <FaLinkedin className='w-6 h-6 hover:scale-110 transition-transform ease-out' />
-                    </a>
+                <div className='relative w-64 -md:h-64 md:w-auto rounded-full z-0 backgroundMainImage'>
+                    <img
+                        className={`absolute top-0 rotate-45 -z-10 scale-[1.35] -translate-y-4 cursor-pointer ${fall}`}
+                        src={fallImg}
+                        alt="Plane image of an autumn leaf"
+                        onClick={() => setFall("animate-fall")}
+                    />
+                    <img
+                        className='h-[91.666667%] w-11/12 m-[4%] rounded-full shadow-xl'
+                        src={profileImg}
+                        alt="Profile picture of Victor Carreño"
+                    />
                 </div>
-                <div className='ml-0 md:ml-9 my-4 text-center md:text-left'>
-                    <a href={cvPdf} download>
-                        <button className='text-amber-900 font-bold bg-amber-100 border-2 border-amber-600 rounded-2xl px-3 py-1 shadow-md active:scale-105 hover:scale-105 hover:shadow-lg transition-transform ease-out' type='button'>
-                            Download CV
-                        </button>
-                    </a>
-                </div>
-            </div>
-            <div className='relative w-64 -md:h-64 md:w-auto rounded-full z-0 backgroundMainImage'>
-                <img
-                    className={`absolute top-0 rotate-45 -z-10 scale-[1.35] -translate-y-4 cursor-pointer ${fall}`}
-                    src={fallImg}
-                    alt="Plane image of an autumn leaf"
-                    onClick={() => setFall("animate-fall")}
-                />
-                <img
-                    className='h-[91.666667%] w-11/12 m-[4%] rounded-full shadow-xl'
-                    src={profileImg}
-                    alt="Profile picture of Victor Carreño"
-                />
             </div>
         </section>
-  )
+    )
 }
 
 export default Home
